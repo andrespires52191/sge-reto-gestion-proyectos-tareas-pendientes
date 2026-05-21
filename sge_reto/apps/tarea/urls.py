81911677a5971f35047eccfd9be9237cb29bc120
148fa12 <andres.pires.52191@ikasle.egibide.org>
2026-05-21 21:10:13 +0200

    Añadir función para filtrar tareas por proyecto
    (cherry picked from commit 104be278bcc8ae27269717f233bff300a9ca79ab)

--- sge_reto/apps/tarea/urls.py
+++ sge_reto/apps/tarea/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, views_api
 
 app_name = 'tarea'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('mostrar/<int:id>/', views.mostrar_tarea, name='mostrar_tarea'),
     path('editar/<int:id>/', views.editar_tarea, name='editar_tarea'),
     path('borrar/<int:id>/', views.borrar_tarea, name='borrar_tarea'),
+
+    path('api/tareas_por_proyecto/<int:id_proyecto>/', views_api.api_tareas_por_proyecto, name='tareas_por_proyecto'),
 ]
--- sge_reto/apps/tarea/views_api.py
+++ sge_reto/apps/tarea/views_api.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Dependencia, Tarea
 from .serializers import DependenciaSerializer, TareaSerializer
@@ -12,3 +14,13 @@ class TareaViewSet(viewsets.ModelViewSet):
 class DependenciaViewSet(viewsets.ModelViewSet):
     queryset = Dependencia.objects.all()
     serializer_class = DependenciaSerializer
+
+@api_view(['GET'])
+def api_tareas_por_proyecto(request, id_proyecto):
+    if id_proyecto == 0:
+        tareas = Tarea.objects.all()
+    else:
+        tareas = Tarea.objects.filter(proyecto_asociado_id=id_proyecto)
+    tareas = tareas.order_by('-fecha_inicio')
+    serializer = TareaSerializer(tareas, many=True)
+    return Response(serializer.data)
